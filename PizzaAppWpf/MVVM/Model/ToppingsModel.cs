40be3886259ec4c7e6677a622bec84eb871b339b
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-17 13:36:09 +0200

    Created an epmty constructor and added a price property

--- PizzaAppWpf/MVVM/Model/ToppingsModel.cs
+++ PizzaAppWpf/MVVM/Model/ToppingsModel.cs
@@ -4,20 +4,27 @@ namespace PizzaAppWpf.Model
 {
     public class ToppingsModel : ICloneable
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool IsSelected { get; set; }
+        public int    Id         { get; set; }
+        public string Name       { get; set; }
+        public int    Price      { get; set; } 
+        public bool   IsSelected { get; set; }
 
-        public ToppingsModel(int id, string name, bool isSelected)
+        public ToppingsModel(int id, string name,int price, bool isSelected)
         {
             Id = id;
             Name = name;
+            Price = price;
             IsSelected = isSelected;
+        }
+
+        public ToppingsModel()
+        {
+            // Empty constructor
         }
 
         public object Clone()
         {
-            return new ToppingsModel(Id, Name, IsSelected);
+            return new ToppingsModel(Id, Name, Price,IsSelected);
         }
     }
 }
