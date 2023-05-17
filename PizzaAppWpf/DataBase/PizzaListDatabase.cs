3be4bec71745fb24c97a771c4127bf65527f07da
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-17 13:27:43 +0200

    Created a method that adds all pizzas in a jsonfile into the database

--- PizzaAppWpf/DataBase/PizzaListDatabase.cs
+++ PizzaAppWpf/DataBase/PizzaListDatabase.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Channels;
 using System.Windows;
+using Newtonsoft.Json;
 using PizzaAppWpf.Model;
 using PizzaAppWpf.MVVM.Model;
 
@@ -48,4 +51,58 @@ partial class Database
     }
 
     public ObservableCollection<PizzaModel> GetAllPizzas() => this._pizzaList;
+
+    private void addPizzasToDBFromJson(string jsonFilePath)
+    {
+        try
+        {
+            ObservableCollection<PizzaModel>? _pizzas =
+                JsonConvert.DeserializeObject<ObservableCollection<PizzaModel>>(
+                    File.ReadAllText($@"{jsonFilePath}"));
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show("Couldn't read data from the given file");
+        }
+
+        try
+        {
+            string connectionString =
+                @"Data Source = SIMPORD\SQLEXPRESS; Initial Catalog=PizzaApp; Integrated Security=True;";
+
+
+            // Create a connection to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Open the connection
+                connection.Open();
+
+                // Iterate over each pizza in the _pizzas collection
+                foreach (PizzaModel pizza in _pizzas)
+                {
+                    // Create a SQL command for inserting the pizza into the table
+                    string sql = "INSERT INTO dbo.PizzasList (imageUrl, description, name, price) " +
+                                 "VALUES (@ImageUrl, @Description, @Name, @Price)";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        // Set the parameter values for the SQL command
+                        command.Parameters.AddWithValue("@ImageUrl", pizza.imageUrl);
+                        command.Parameters.AddWithValue("@Description", pizza.Description);
+                        command.Parameters.AddWithValue("@Name", pizza.Name);
+                        command.Parameters.AddWithValue("@Price", pizza.Price);
+
+                        // Execute the SQL command
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            System.Console.WriteLine("Yay", Console.ForegroundColor = ConsoleColor.Green);
+        }
+        catch (System.Exception e)
+        {
+            System.Console.WriteLine($"Ohh No \n{e.Message} ", Console.ForegroundColor = ConsoleColor.Red);
+        }
+    }
 }
