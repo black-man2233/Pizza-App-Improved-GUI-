064f7a19f9dee14dcb937c2a83e05d7c6830914d
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-22 08:55:01 +0200

    Capacities retrieved from db

--- PizzaAppWpf/MVVM/Model/ToppingsSizeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable
-namespace PizzaAppWpf.MVVM.Model
-{
-    class ToppingsSizeModel
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
