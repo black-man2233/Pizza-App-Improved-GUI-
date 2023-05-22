c81921eefcb8123d96f26a0fd5fe299b3c457cbb
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-22 11:11:14 +0200

    Added DrinksSizes to drinks

--- PizzaAppWpf/DataBase/DrinksDatabase.cs
+++ PizzaAppWpf/DataBase/DrinksDatabase.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.SqlClient;
 using System.Windows;
@@ -10,23 +11,52 @@ namespace PizzaAppWpf.DataBase;
 partial class Database
 {
     private ObservableCollection<DrinkModel> _drinksList = new();
+    public ObservableCollection<DrinkModel> GetAllDrinks() => _drinksList;
 
-    private void GetDrinksFromDB(SqlConnection connection)
+    private void GetDrinksFromDb(SqlConnection connection)
     {
+        // Get Drink Sizes
+        List<CapacitiesModel> drinkSizes = new();
         try
         {
-            SqlCommand command = new SqlCommand("SELECT * FROM dbo.Drinks", connection);
-
             connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
+            SqlCommand drinksSizeCommand = new SqlCommand("SELECT * FROM Capacities", connection);
+            SqlDataReader sizeReader = drinksSizeCommand.ExecuteReader();
 
-            while (reader.Read())
+            while (sizeReader.Read())
+            {
+                CapacitiesModel drinkSize = new()
+                {
+                    Id = (int)sizeReader["ID"],
+                    Name = sizeReader["Name"].ToString(),
+                    Price = (int)sizeReader["Price"]
+                };
+                drinkSizes.Add(drinkSize);
+            }
+
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show($"Error: {e.Message} \n Getting Drink Sizes From DB");
+        }
+
+        try
+        {
+            SqlCommand drinksCommand = new SqlCommand("SELECT * FROM dbo.Drinks", connection);
+            connection.Open();
+            SqlDataReader drinksReader = drinksCommand.ExecuteReader();
+
+
+            // Get Drinks
+            while (drinksReader.Read())
             {
                 DrinkModel drink = new()
                 {
-                    imageUrl = reader["imageUrl"].ToString(),
-                    Name = reader["Name"].ToString(),
-                    Description = reader["Description"].ToString()
+                    imageUrl = drinksReader["imageUrl"].ToString(),
+                    Name = drinksReader["Name"].ToString(),
+                    Description = drinksReader["Description"].ToString(),
+                    Size = drinkSizes
                 };
                 _drinksList.Add(drink);
             }
