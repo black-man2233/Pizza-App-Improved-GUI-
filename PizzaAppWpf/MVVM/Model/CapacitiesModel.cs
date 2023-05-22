521308d3da50aafae2113f1ee8607747e6ea36a4
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-22 11:10:37 +0200

    Created an empty COnstructor

--- PizzaAppWpf/MVVM/Model/CapacitiesModel.cs
+++ PizzaAppWpf/MVVM/Model/CapacitiesModel.cs
@@ -5,5 +5,9 @@ public class CapacitiesModel
     public int Id { get; set; }
     public string Name  { get; set; }
     public int    Price { get; set; }
-    
+
+    public CapacitiesModel()
+    {
+        // Empty constructor
+    }
 }
