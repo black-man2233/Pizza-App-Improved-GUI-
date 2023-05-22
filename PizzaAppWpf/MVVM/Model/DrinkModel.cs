9c625eb49483328bde118e43ea5b02ef2b1df7ba
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-22 11:12:17 +0200

    Added a Size List

--- PizzaAppWpf/MVVM/Model/DrinkModel.cs
+++ PizzaAppWpf/MVVM/Model/DrinkModel.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 
+#pragma warning disable
 namespace PizzaAppWpf.MVVM.Model;
 
 public class DrinkModel : ICloneable
@@ -7,6 +9,7 @@ public class DrinkModel : ICloneable
     public string imageUrl    { get; set; }
     public string Name        { get; set; }
     public string Description { get; set; }
+    public List<CapacitiesModel> Size        { get; set; }
 
     public DrinkModel()
     {
@@ -15,16 +18,17 @@ public class DrinkModel : ICloneable
         Description = "";
     }
 
-    public DrinkModel(string imageUrl, string name, string description)
+    public DrinkModel(string imageUrl, string name, string description, object size)
     {
         this.imageUrl = imageUrl;
         Name = name;
         Description = description;
+        Size = (List<CapacitiesModel>)size;
     }
 
 
     public object Clone()
     {
-        return new DrinkModel(imageUrl, Name, Description);
+        return new DrinkModel(imageUrl, Name, Description, this.Size);
     }
 }
